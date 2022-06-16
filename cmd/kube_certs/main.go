f7c2ed1e73f345c7b278a2b8005cb46b7e40127b
LinQing2017 <m15651620957@163.com>
2022-06-16 21:45:15 +0800

    feat: kubernetes 证书自动生成

--- cmd/kube_certs/main.go
+++ cmd/kube_certs/main.go
@@ -1,18 +1,36 @@
 package main
 
 import (
+	"crypto"
 	cryptorand "crypto/rand"
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"github.com/QQGoblin/go-sdk/pkg/pkiutil"
 	"github.com/pkg/errors"
+	"k8s.io/client-go/tools/clientcmd"
+	"k8s.io/client-go/util/keyutil"
 	"k8s.io/klog/v2"
 	"math"
 	"math/big"
+	"path"
 	"time"
 )
 
 func main() {
+	if err := CreateClientCertAndKeyToDisk("tls", "ca", "kube-apiserver-kubelet-client", []string{pkiutil.SystemPrivilegedGroup}); err != nil {
+		klog.Fatalf("create kube-apiserver-kubelet-client tls pki failed: %s", err.Error())
+	}
+
+	if err := CreateClientCertAndKeyToDisk("tls", "ca", "front-proxy-client", nil); err != nil {
+		klog.Fatalf("create kube-apiserver-kubelet-client tls pki failed: %s", err.Error())
+	}
+
+	if err := CreateKubeconfig("tls", "ca", "https://kubernetes.lb", "https://127.0.0.1:6443", "node1"); err != nil {
+		klog.Fatalf("create kubernetes config failed: %s", err.Error())
+	}
+}
+
+func CreateSignedCertAndKey(commonName string, organization []string, caCert *x509.Certificate, caKey crypto.Signer, extKeyUsage []x509.ExtKeyUsage) (*x509.Certificate, crypto.Signer, error) {
 
 	notBefore, _ := time.Parse("2006-01-02 15:04:05", "1970-01-01 00:00:00")
 	notAfter, _ := time.Parse("2006-01-02 15:04:05", "2170-01-01 00:00:00")
@@ -20,49 +38,83 @@ func main() {
 
 	certTmpl := &x509.Certificate{
 		Subject: pkix.Name{
-			CommonName: "front-proxy-client",
+			CommonName:   commonName,
+			Organization: organization,
 		},
-		DNSNames:              nil,
-		IPAddresses:           nil,
 		SerialNumber:          serial,
 		NotBefore:             notBefore,
 		NotAfter:              notAfter,
 		KeyUsage:              x509.KeyUsageKeyEncipherment | x509.KeyUsageDigitalSignature,
-		ExtKeyUsage:           []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth},
+		ExtKeyUsage:           extKeyUsage,
 		BasicConstraintsValid: true,
 		IsCA:                  false,
 	}
-
-	if err := NewClientCertAndKeyToDisk(certTmpl, "tls/", "ca", "front-proxy-client"); err != nil {
-		klog.Fatal(err.Error())
+	key, err := pkiutil.NewPrivateKey(x509.RSA)
+	if err != nil {
+		return nil, nil, err
 	}
-}
 
-func NewClientCertAndKeyToDisk(certTmpl *x509.Certificate, pkiPath, caName, certName string) error {
+	certDERBytes, err := x509.CreateCertificate(cryptorand.Reader, certTmpl, caCert, key.Public(), caKey)
+	if err != nil {
+		return nil, nil, err
+	}
+	cert, err := x509.ParseCertificate(certDERBytes)
+	if err != nil {
+		return nil, nil, err
+	}
+	return cert, key, nil
+
+}
+func CreateClientCertAndKeyToDisk(pkiPath, ca, certs string, organization []string) error {
+
+	caCert, caKey, err := pkiutil.TryLoadCertAndKeyFromDisk(pkiPath, ca)
+	if err != nil {
+		return errors.Wrapf(err, "error load ca from disk: %s", err.Error())
+	}
+
+	cert, key, err := CreateSignedCertAndKey(certs, organization, caCert, caKey, []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth})
+
+	if err != nil {
+		return errors.Wrapf(err, "error create certificate and key: %s", err.Error())
+	}
+	if err := pkiutil.WriteCertAndKey(pkiPath, certs, cert, key); err != nil {
+		return errors.Wrapf(err, "error write certificate and key file: %s", err.Error())
+	}
+
+	return nil
+}
+func CreateKubeconfig(pkiPath, caName, controlPlaneEndpoint, localAPIEndpoint, nodeName string) error {
 
 	caCert, caKey, err := pkiutil.TryLoadCertAndKeyFromDisk(pkiPath, caName)
 	if err != nil {
 		return errors.Wrapf(err, "error load ca from disk: %s", err.Error())
 	}
 
-	key, err := pkiutil.NewPrivateKey(x509.RSA)
-	if err != nil {
-		return errors.Wrapf(err, "error create key: %s", err.Error())
-	}
+	configSpecs := pkiutil.GetKubeConfigSpecsBase(controlPlaneEndpoint, localAPIEndpoint, nodeName)
 
-	certDERBytes, err := x509.CreateCertificate(cryptorand.Reader, certTmpl, caCert, key.Public(), caKey)
-	if err != nil {
-		return errors.Wrapf(err, "error create certificate: %s", err.Error())
-	}
+	for name, configSpec := range configSpecs {
+		cert, key, err := CreateSignedCertAndKey(configSpec.ClientName, configSpec.ClientCertAuth.Organizations, caCert, caKey, []x509.ExtKeyUsage{x509.ExtKeyUsageClientAuth})
+		if err != nil {
+			return errors.Wrapf(err, "error create certificate and key: %s", err.Error())
+		}
 
-	cert, err := x509.ParseCertificate(certDERBytes)
-	if err != nil {
-		return errors.Wrapf(err, "error parse certificate: %s", err.Error())
-	}
+		encodedClientKey, err := keyutil.MarshalPrivateKeyToPEM(key)
+		if err != nil {
+			return errors.Wrapf(err, "failed to marshal private key to PEM")
+		}
+		kubeconfig := pkiutil.CreateWithCerts(
+			configSpec.APIServer,
+			"cluster.local",
+			configSpec.ClientName,
+			pkiutil.EncodeCertPEM(caCert),
+			encodedClientKey,
+			pkiutil.EncodeCertPEM(cert),
+		)
 
-	if err := pkiutil.WriteCertAndKey(pkiPath, certName, cert, key); err != nil {
-		return errors.Wrapf(err, "error write certificate and key file: %s", err.Error())
+		filename := path.Join(pkiPath, name)
+		if err := clientcmd.WriteToFile(*kubeconfig, filename); err != nil {
+			return errors.Wrapf(err, "error write kubeconfig file to disk: %s", err.Error())
+		}
 	}
-
 	return nil
 }
---
+++ pkg/pkiutil/kubeconfig_helpers.go
@@ -0,0 +1,120 @@
+package pkiutil
+
+import (
+	"crypto"
+	"crypto/x509"
+	"fmt"
+	clientcmdapi "k8s.io/client-go/tools/clientcmd/api"
+)
+
+const (
+	// AdminKubeConfigFileName defines name for the kubeconfig aimed to be used by the superuser/admin of the cluster
+	AdminKubeConfigFileName = "admin.conf"
+	// KubeletKubeConfigFileName defines the file name for the kubeconfig that the control-plane kubelet will use for talking
+	// to the API server
+	KubeletKubeConfigFileName = "kubelet.conf"
+	// ControllerManagerKubeConfigFileName defines the file name for the controller manager's kubeconfig file
+	ControllerManagerKubeConfigFileName = "controller-manager.conf"
+	// SchedulerKubeConfigFileName defines the file name for the scheduler's kubeconfig file
+	SchedulerKubeConfigFileName = "scheduler.conf"
+)
+
+const (
+	// ControllerManagerUser defines the well-known user the controller-manager should be authenticated as
+	ControllerManagerUser = "system:kube-controller-manager"
+	// SchedulerUser defines the well-known user the scheduler should be authenticated as
+	SchedulerUser = "system:kube-scheduler"
+	// SystemPrivilegedGroup defines the well-known group for the apiservers. This group is also superuser by default
+	// (i.e. bound to the cluster-admin ClusterRole)
+	SystemPrivilegedGroup = "system:masters"
+	// NodesGroup defines the well-known group for all nodes.
+	NodesGroup = "system:nodes"
+	// NodesUserPrefix defines the user name prefix as requested by the Node authorizer.
+	NodesUserPrefix = "system:node:"
+	// NodesClusterRoleBinding defines the well-known ClusterRoleBinding which binds the too permissive system:node
+	// ClusterRole to the system:nodes group. Since kubeadm is using the Node Authorizer, this ClusterRoleBinding's
+	// system:nodes group subject is removed if present.
+	NodesClusterRoleBinding = "system:node"
+)
+
+// clientCertAuth struct holds info required to build a client certificate to provide authentication info in a kubeconfig object
+type clientCertAuth struct {
+	CAKey         crypto.Signer
+	Organizations []string
+}
+
+// tokenAuth struct holds info required to use a token to provide authentication info in a kubeconfig object
+type tokenAuth struct {
+	Token string `datapolicy:"token"`
+}
+
+// kubeConfigSpec struct holds info required to build a KubeConfig object
+type kubeConfigSpec struct {
+	CACert         *x509.Certificate
+	APIServer      string
+	ClientName     string
+	TokenAuth      *tokenAuth      `datapolicy:"token"`
+	ClientCertAuth *clientCertAuth `datapolicy:"security-key"`
+}
+
+func GetKubeConfigSpecsBase(controlPlaneEndpoint, localAPIEndpoint, nodeName string) map[string]*kubeConfigSpec {
+	return map[string]*kubeConfigSpec{
+		AdminKubeConfigFileName: {
+			APIServer:  controlPlaneEndpoint,
+			ClientName: "kubernetes-admin",
+			ClientCertAuth: &clientCertAuth{
+				Organizations: []string{SystemPrivilegedGroup},
+			},
+		},
+		KubeletKubeConfigFileName: {
+			APIServer:  controlPlaneEndpoint,
+			ClientName: fmt.Sprintf("%s%s", NodesUserPrefix, nodeName),
+			ClientCertAuth: &clientCertAuth{
+				Organizations: []string{NodesGroup},
+			},
+		},
+		ControllerManagerKubeConfigFileName: {
+			APIServer:      localAPIEndpoint,
+			ClientName:     ControllerManagerUser,
+			ClientCertAuth: &clientCertAuth{},
+		},
+		SchedulerKubeConfigFileName: {
+			APIServer:      localAPIEndpoint,
+			ClientName:     SchedulerUser,
+			ClientCertAuth: &clientCertAuth{},
+		},
+	}
+}
+
+// CreateBasic creates a basic, general KubeConfig object that then can be extended
+func CreateBasic(serverURL, clusterName, userName string, caCert []byte) *clientcmdapi.Config {
+	// Use the cluster and the username as the context name
+	contextName := fmt.Sprintf("%s@%s", userName, clusterName)
+
+	return &clientcmdapi.Config{
+		Clusters: map[string]*clientcmdapi.Cluster{
+			clusterName: {
+				Server:                   serverURL,
+				CertificateAuthorityData: caCert,
+			},
+		},
+		Contexts: map[string]*clientcmdapi.Context{
+			contextName: {
+				Cluster:  clusterName,
+				AuthInfo: userName,
+			},
+		},
+		AuthInfos:      map[string]*clientcmdapi.AuthInfo{},
+		CurrentContext: contextName,
+	}
+}
+
+// CreateWithCerts creates a KubeConfig object with access to the API server with client certificates
+func CreateWithCerts(serverURL, clusterName, userName string, caCert []byte, clientKey []byte, clientCert []byte) *clientcmdapi.Config {
+	config := CreateBasic(serverURL, clusterName, userName, caCert)
+	config.AuthInfos[userName] = &clientcmdapi.AuthInfo{
+		ClientKeyData:         clientKey,
+		ClientCertificateData: clientCert,
+	}
+	return config
+}
