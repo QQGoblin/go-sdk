f2f836313ceeceab59cddec08e71c32fc8000b19
linqing <linqing@ruijie.com.cn>
2022-08-07 16:21:29 +0800

    fix: 调整目录结构

--- hack/gobuild.sh
+++ hack/gobuild.sh
@@ -3,4 +3,4 @@ export GOOS=linux
 export GOARCH=amd64
 export CGO_ENABLED=0
 export GOPROXY=https://goproxy.cn,direct
-go build -o helmimg cmd/helm/images.go
+go build -o helmimg example/helm/images.go
